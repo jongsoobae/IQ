44965128d1e9b5769a96b4cfd1e956cabb746144
jongsoo.bae <jongsoo.bae@deliveryhero.co.kr>
2020-02-27 13:57:41 +0900

    fix title

--- iq-client/nuxt.config.js
+++ iq-client/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: '%s - ' + process.env.npm_package_description,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
