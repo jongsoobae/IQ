e28f9c8057b5aa0d504538fcebe2205e2cde8670
jongsoobae <ocpek@nate.com>
2020-04-15 01:03:41 +0900

    if no additional_tags, do not insert.

--- iq-api/app/mongo/compose.py
+++ iq-api/app/mongo/compose.py
@@ -6,4 +6,5 @@ from app.mongo import tag
 async def add_additional_tags(db, tags: List[str]):
     existing_tags = await tag.find(db)
     additional_tags = set(tags).difference(x["name"] for x in existing_tags)
-    await tag.insert_many(db, [{"name": tag} for tag in additional_tags])
+    if additional_tags:
+        await tag.insert_many(db, [{"name": tag} for tag in additional_tags])
